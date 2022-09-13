c2a0d214691a5b10e237d49c5bc36fa939ecc403
PaoTorres <polato22@yahoo.com>
2022-09-12 21:19:31 -0500

    Commit at 21:20 hours

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react'
+import './App.css'
+import ResidentInfo from './components/ResidentInfo';
+
+function App() {
+  const [rickmorty, setRickMorty] = useState ({})
+  const [typeId, setTypeId] = useState("");
+
+ useEffect(()=>{
+ const randomId = Math.floor(Math.random()*125)+1
+ axios.get(`https://rickandmortyapi.com/api/location/${randomId}`)
+      .then(res=>setRickMorty(res.data));
+ }, []);
+
+ console.log(rickmorty);
+
+ const searchType=()=>{
+      axios.get(`https://rickandmortyapi.com/api/location/${typeId}`)
+           .then(res=>setRickMorty(res.data));
+}
+
+
+  return (
+    <div className="App">
+      <div className='imgBG'>
+       <input
+          type="text"  
+          value={typeId}
+          placeholder= "Type a location ID"
+          onChange={e =>setTypeId(e.target.value)}
+       /> 
+       <button onClick={searchType} className='btn'><span class="material-symbols-outlined">search</span></button>
+      </div>
+      <h2>{rickmorty.name}</h2>
+      <ul className='text'>
+       <li className='color'><b>Type:</b> {rickmorty.type}</li>
+       <li className='color'><b>Dimension:</b> {rickmorty.dimension}</li>
+       <li className='color'><b>Population:</b> {rickmorty.residents?.length}</li>
+      </ul>
+      <br />
+      <h2>Residents</h2>
+        <ul className='card'> {rickmorty.residents?.map((resident) => (
+              <ResidentInfo url={resident} key={resident} />
+
+              ))}
+        </ul>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/ResidentInfo.jsx
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+
+const ResidentInfo = ({url}) => {
+
+    const [character, setCharacter] = useState ({})
+
+    useEffect(()=>{
+        axios.get(`${url}`)
+             .then(res=>setCharacter(res.data));
+        }, []);
+
+    console.log(character)
+
+    return (
+        <li className='size'>
+            <div className="ctn">
+                <img src={character.image} alt="" />
+                <h3>{character?.name}</h3>
+                <p><b>Status:</b> {character.status}</p>
+                <p><b>Origin:</b> {character.origin?.name}</p>
+                <p><b>Episodes where appear:</b> {character.episode?.length}</p>
+            </div>
+        </li>
+    );
+};
+
+export default ResidentInfo;
